2245bc4e80e163e926dbeab5c0ef108b7d9cad3e
Daniel Nguyen <trung.daniel.nguyen@gmail.com>
2026-08-19 22:53:26 -0400

    added policy for KEDA and named the resource

---
+++ terraform/keda-iam.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "keda_operator_irsa_role"  {
+    name = "keda-operator-irsa-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Action = "sts:AssumeRoleWithWebIdentity"
+                Principal = {
+                    Federated = aws_iam_openid_connect_provider.pulsedesk_connect_provider.arn 
+                }
+                Condition = {
+                    StringEquals = {
+                        "${replace(aws_iam_openid_connect_provider.pulsedesk_connect_provider.url, "https://", "")}:sub" = "system:serviceaccount:keda:keda-operator"
+                        "${replace(aws_iam_openid_connect_provider.pulsedesk_connect_provider.url, "https://", "")}:aud" = "sts.amazonaws.com"
+                    }
+                }
+            }
+        ]
+    })
+}
