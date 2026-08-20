3a60cb80d960d8d004a78e934c96c0daf5034bad
Daniel Nguyen <danielnguyen@mac.home>
2026-08-19 20:52:58 -0400

    added 2 more t2.micos for KEDA pods setup

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -15,9 +15,9 @@ resource "aws_eks_node_group" "pulsedesk_node_group" {
     instance_types = ["t3.micro"]
 
     scaling_config {
-        desired_size = 2
-        max_size = 2
-        min_size = 1
+        desired_size = 3
+        max_size = 3
+        min_size = 3
     }
     
     depends_on = [
